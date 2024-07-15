4cb585998d3441f535fbb15b7a6eb2caf1991cba
“Ries <ries@vriend.com>
2024-07-15 18:41:30 +0200

    x

--- models/riverflow_workflow_state.py
+++ models/riverflow_workflow_state.py
@@ -16,7 +16,7 @@ class RiverflowWorkflowState(models.Model):
     workflow_id = fields.Many2one(
         'riverflow.workflow', 'Workflow', copy=True, index=True, required=True)
     sequence = fields.Integer(default=10)
-    isEndState = fields.Boolean('Is End State', default=False)
+    is_end_state = fields.Boolean('Is End State', default=False)
 
     from_transition_ids = fields.One2many(
         'riverflow.workflow.transition', 'from_state_id', string='From-transitions', help='Transitions from this state')
