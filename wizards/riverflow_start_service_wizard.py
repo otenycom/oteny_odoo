3095404f03d8e00dd3b6edfdf481defb7550983d
“Ries <ries@vriend.com>
2024-08-01 11:39:35 +0200

    start trans

---
+++ wizards/riverflow_start_service_wizard.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+
+class ServiceNewWizard(models.TransientModel):
+    _name = 'riverflow.start.service'
+    _inherit = 'riverflow.start.wizard'
+    _description = 'Service Start Transition selection Wizard'
+
+    _workflow_model = 'riverflow.service'
