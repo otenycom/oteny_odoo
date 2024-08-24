54b9005eec426f36dfee38d5f1a2c073099fc337
“Ries <ries@vriend.com>
2024-08-24 22:00:49 +0200

    allowed_domains

---
+++ models/res_config_settings.py
@@ -0,0 +1,11 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    restrict_email_recipients_to = fields.Char(
+        string="Restrict Email Recipients To",
+        config_parameter="riverflow.restrict_email_recipients_to",
+        help="Comma-separated list of allowed email domains (e.g., cuneus.eu,vriend.com)",
+    )
