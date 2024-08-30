9f3c185a90ce4c94a45a09000f703ecc726b2aad
“Ries <ries@vriend.com>
2024-08-30 21:18:44 +0200

    cleanup work_status,autoadd

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "17.0.1.111",
+    "version": "17.0.1.113",
     "depends": ["base", "mail"],
     "author": "Vriend Studio",
     "category": "Workflow",
