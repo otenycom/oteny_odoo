ca419a19bfb10cec5325fd24002d969e466c14f2
“Ries <ries@vriend.com>
2024-08-31 13:05:56 +0200

    Make selecting a transition from a long list of transitions easy

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "17.0.1.121",
+    "version": "17.0.1.123",
     "depends": ["base", "mail"],
     "author": "Vriend Studio",
     "category": "Workflow",
