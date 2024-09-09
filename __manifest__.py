2ba0e84b7f9353604b501443243cc93c1db21485
“Ries <ries@vriend.com>
2024-09-09 21:55:19 +0200

    no doc holder check

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "17.0.1.147",
+    "version": "17.0.1.149",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
