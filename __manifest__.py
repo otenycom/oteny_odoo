6ff82927e333d4758bbc750b66b1957733f9c640
“Ries <ries@vriend.com>
2024-09-16 18:17:59 +0200

    fixes for document checks

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "17.0.1.181",
+    "version": "17.0.1.183",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
--- models/models.py
+++ models/models.py
@@ -5,6 +5,7 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+"""
 
 class BaseModel(models.AbstractModel):
     _inherit = "base"
@@ -19,11 +20,10 @@ class BaseModel(models.AbstractModel):
         if next_id:
             self = self.browse(next_id)
 
-        """
-        HACK: Workaround for client not receiving updates on check results after create/write
-        TODO: fix in code so that web_read sees invalidated fields and does the recompute 
-        .... See also riverflow_check_results_mixin.py _write()
-        """
+        # HACK: Workaround for client not receiving updates on check results after create/write
+        # TODO: fix in code so that web_read sees invalidated fields and does the recompute 
+        # .... See also riverflow_check_results_mixin.py _write()
         self.env.flush_all()
 
         return self.with_context(bin_size=True).web_read(specification)
+"""
