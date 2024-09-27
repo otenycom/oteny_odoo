3c513389b8355221c88f8adcb8e3daf607f951d9
“Ries <ries@vriend.com>
2024-09-28 01:22:51 +0200

    calendar view

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "17.0.1.221",
+    "version": "17.0.1.223",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
