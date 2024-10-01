3f4f985ddd44eed75a1c33860b342ede3f353702
“Ries <ries@vriend.com>
2024-10-01 12:52:15 +0200

    fix filters overdue

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "17.0.1.235",
+    "version": "17.0.1.237",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
