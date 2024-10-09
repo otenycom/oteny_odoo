949780805de95bfb6e6cdca65b78fd63dcc8a7e8
“Ries <ries@vriend.com>
2024-10-09 19:59:51 +0200

    new button / count

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.275",
+    "version": "18.0.1.276",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
