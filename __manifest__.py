9e7daac936c1dcf0d8ad31918796dbd483cdb313
“Ries <ries@vriend.com>
2024-10-09 15:23:40 +0200

    x

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.268",
+    "version": "18.0.1.270",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
