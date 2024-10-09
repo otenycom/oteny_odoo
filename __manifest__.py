2bae813263b14a575e62899d90205a03882c8d94
“Ries <ries@vriend.com>
2024-10-09 12:52:47 +0200

    fix widht https://3.basecamp.com/5527227/buckets/38887735/todos/7898090388

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.265",
+    "version": "18.0.1.266",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
