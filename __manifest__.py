9149ffb896f598aca16c2d2a82553286f80728fb
“Ries <ries@vriend.com>
2024-10-15 21:28:51 +0200

    fix template

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.293",
+    "version": "18.0.1.294",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
