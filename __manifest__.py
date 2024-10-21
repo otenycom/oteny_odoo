25dc6ef90db2594856e26c030991b99655fe1c66
“Ries <ries@vriend.com>
2024-10-21 20:58:15 +0200

    fix import, workflows, document view creds

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.294",
+    "version": "18.0.1.295",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
