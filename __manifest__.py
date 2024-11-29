556b2a657d80b93fb854360d9bbf7f477c2e2616
Ries Vriend <ries@vriend.com>
2024-11-29 10:02:53 +0100

    y

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.302",
+    "version": "18.0.1.303",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
--- wizards/riverflow_service_wizard.py
+++ wizards/riverflow_service_wizard.py
@@ -1,7 +1,6 @@
 from odoo import models, fields, api
 
 
-#
 class ServiceWizard(models.TransientModel):
     _name = "riverflow.service.wizard"
     _inherit = "riverflow.transition.wizard"
