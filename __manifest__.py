27f2caea905aebbf886f88e78315b9fb436c68da
Ries Vriend <ries@vriend.com>
2024-12-11 13:48:36 +0100

    feat: staging changes from staging

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.315",
+    "version": "18.0.1.316",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
