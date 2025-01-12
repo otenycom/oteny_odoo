f6829884dd37b6d89edc9d29a2007018e103e663
Ries Vriend <ries@vriend.com>
2025-01-12 15:04:03 +0100

    Update workflow names and state configurations in riverflow module
    - Changed the workflow name from "Placement" to "Work" in log_entry_workflow_crew_change.xml for better clarity.
    - Updated the workflow name in log_entry_workflow_work.xml from "Work" to "Work (Old)" to differentiate between versions.
    - Modified the is_end_state field in service_book_flight.xml from True to False, indicating that the "Sent" state is not an end state.
    - Enhanced the LogEntry model in rivermen_log_entry.py to conditionally display start and end dates based on the workflow status, improving the relevance of displayed information.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.349",
+    "version": "18.0.1.350",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
