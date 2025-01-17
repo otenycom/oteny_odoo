1f879d42cb878bf8ead04db9a45edcc4e0d115f3
Ries Vriend <ries@vriend.com>
2025-01-17 15:54:39 +0100

    Update condition domain in riverflow_auto_add_service_data.xml for service auto-add logic
    - Changed False condition_domain from [(1, '=', 0)] to [(0, '=', 1)] to correct the logic for auto-adding // fixes error evaluating this domain
    - This adjustment ensures that the service is triggered correctly when the employee lacks the 'riverbook' credential.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.358",
+    "version": "18.0.1.359",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
