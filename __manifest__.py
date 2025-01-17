789817b8f27a9b553f15c8887fc5e9498a6b1c34
Ries Vriend <ries@vriend.com>
2025-01-17 15:41:46 +0100

    Refactor billing wizard to improve log entry handling and invoice creation
    - Implemented a set-based approach to avoid duplicate billable entries.
    - Enhanced filtering logic for billable entries by ship and employee, ensuring accurate data retrieval.
    - Introduced a new field 'log_entry_ids' in BillingSummary to store associated log entries for better tracking.
    - Updated invoice creation logic to utilize stored log_entry_ids, improving the association between invoices and billing lines.
    - Refined the description formatting in billing summaries for clarity.
    - Ensured robust handling of empty entry cases to prevent errors during processing.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.357",
+    "version": "18.0.1.358",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
