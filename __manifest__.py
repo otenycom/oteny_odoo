afec5c2ef0d212c794064d9027acee57d17936f7
Thijs van Waaij <thijs@vriend.studio>
2025-01-20 15:54:55 +0100

    Add Rivermen Rate model and views; refactor AccountMoveLine and BillingWizard
    - Introduced a new model 'rivermen.rate' to manage rates with fields for value, valid dates, and associated types.
    - Added corresponding views for the Rivermen Rate model, including form, list, and pivot views for better user interaction.
    - Refactored 'AccountMoveLine' to replace 'summary_id' with 'summary_identifier' for improved clarity in billing references.
    - Updated 'BillingWizard' to utilize the new identifier field, enhancing the mapping of summary lines to billing lines.
    - Included new imports in the manifest and model initialization files to support the new features.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.360",
+    "version": "18.0.1.361",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
