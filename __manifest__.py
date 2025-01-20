8767c82da425572f62965c725a5b0f4baf29cfb3
Thijs van Waaij <thijs@vriend.studio>
2025-01-20 16:11:56 +0100

    Merge Thijs-dev to acc

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.361",
+    "version": "18.0.1.367",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
