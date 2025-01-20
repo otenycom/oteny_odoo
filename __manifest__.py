17bf6b6f456dbb7bc1a9f8ba01db40ed04389293
Thijs van Waaij <thijs@vriend.studio>
2025-01-20 16:19:44 +0100

    Refactor RivermenRate model by disabling tracking on fields
    - Commented out the tracking attribute on 'rate_type' and 'credential_type_id' fields to simplify change tracking.
    - Added a description to the 'RivermenShipRateType' model for better clarity on its purpose.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.367",
+    "version": "18.0.1.368",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
