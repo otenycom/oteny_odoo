a9ca868c06dc93564837611d2d74807b84dd1d50
Ries Vriend <ries@vriend.com>
2025-01-21 14:31:51 +0100

    Refactor Rivermen rate model and views; enhance billing wizard
    - Updated 'rivermen_rate_data.xml' to replace 'credential_type_id' with 'position_id' for better clarity in rate records.
    - Refactored 'RivermenRate' model to introduce 'lookup_sort_order' for improved rate retrieval logic based on multiple criteria.
    - Enhanced billing wizard to dynamically check for issues related to ship type and employee credentials during rate price lookup.
    - Added new 'BillingIssue' model to track issues encountered during billing preparation, improving error handling and user feedback.
    - Updated views to reflect changes in field names and added new fields for better data representation and user experience.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.370",
+    "version": "18.0.1.371",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
