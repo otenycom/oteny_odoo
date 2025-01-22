417f7480862da36efee69f5a5eb97677755b0cd2
Ries Vriend <ries@vriend.com>
2025-01-22 16:08:46 +0100

    Refactor Rivermen rate model and views to replace 'operator_country_id' with 'debtor_country_id'
    - Updated 'rivermen_rate_data.xml' to change field references from 'operator_country_id' to 'debtor_country_id' for consistency.
    - Refactored 'RivermenRate' model to reflect the new field name and adjusted dependencies accordingly.
    - Modified views in 'rivermen_rate_views.xml' to ensure the new field name is used throughout the UI.
    - Enhanced billing wizard logic to validate against 'debtor_country_id', improving accuracy in rate matching.
    
    These changes aim to clarify the purpose of the country field in the rate model and enhance the overall integrity of the billing process.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.373",
+    "version": "18.0.1.374",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
