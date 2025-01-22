25d917d3356b26599e97448c9536885223616849
Ries Vriend <ries@vriend.com>
2025-01-22 15:51:06 +0100

    Refactor billing wizard logic and tests for improved accuracy and clarity
    - fix test: Removed  summary line computation in the billing wizard form.
    - Updated invoice line verification to match billing line identifiers instead of employee references.
    - Enhanced summary line filtering logic to use identifiers for better consistency.
    - Commented out unnecessary tracking and logging decorators to streamline performance.
    - Improved overall readability and maintainability of the billing logic.
    
    These changes aim to enhance the accuracy of billing calculations and improve the clarity of the codebase.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.372",
+    "version": "18.0.1.373",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
