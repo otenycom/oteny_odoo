71b6ef0cdcd3a7232ae75c1a04393fdcdf991a10
Ries Vriend <ries@vriend.com>
2025-01-23 17:00:54 +0100

    Enhance billing wizard tests and logic for improved error handling and data validation
    - Added tests to verify billing wizard behavior with planned end dates within the billing period, ensuring proper error notifications.
    - Implemented logic in the billing wizard to check for planned end dates and generate corresponding issues, enhancing user feedback.
    - Updated test cases to reflect changes in actual end date handling, improving accuracy in billing scenarios.
    - Created default ship and rate types for testing purposes, ensuring a consistent environment for billing tests.
    
    These changes aim to improve the robustness of the billing process and enhance the clarity of error handling in the Rivermen module.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.378",
+    "version": "18.0.1.379",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
