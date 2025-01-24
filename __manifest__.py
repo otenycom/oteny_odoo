58356752f734f83230a82d12dd5b3110ab73a321
Ries Vriend <ries@vriend.com>
2025-01-24 16:32:04 +0100

    Update VSCode configuration and enhance example settings for Odoo integration
    - Modified '.vscode/launch.json' to use a configuration variable for the 'installModules' setting, improving flexibility in module installation.
    - Updated '.vscode/settings.example.json' to include the 'installModules' key, providing a clearer template for users to configure their Odoo settings.
    - Ensured consistency in the 'importFolder' path formatting.
    
    These changes aim to streamline the development environment setup for Odoo projects and enhance user onboarding experience.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.382",
+    "version": "18.0.1.383",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
