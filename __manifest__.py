f9a6429b542c413e06c9038bb7ea371ffb3a0369
Ries Vriend <ries@vriend.com>
2025-01-24 17:55:58 +0100

    invoice lines per entry, to make billing review easier
    - Simplified the billing logic by removing redundant combinations and directly processing entries, enhancing performance and readability.
    - Improved error handling and user feedback by refining issue descriptions and ensuring accurate representation of planned end dates.
    - Adjusted the BillingSummary model to represent a single log entry, improving data integrity and clarity in billing summaries.
    
    These changes aim to enhance the user experience and efficiency of the billing process within the Rivermen module.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.383",
+    "version": "18.0.1.384",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
