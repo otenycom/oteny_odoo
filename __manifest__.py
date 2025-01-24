7a5da90f9b5e674cf8530efc4e64f49d0b4a191a
Ries Vriend <ries@vriend.com>
2025-01-24 14:25:42 +0100

    Update VSCode configuration and improve import settings
    - Added a comment in '.vscode/launch.json' to clarify the purpose of the PDF import timeout setting, enhancing understanding of its importance in preventing server hangs.
    - Updated the comment in '.vscode/rivermen.code-workspace' to provide clearer instructions on maintaining Python indentation in Odoo XML fields.
    - Corrected the 'addonsPath' in '.vscode/settings.example.json' to remove an extra comma, ensuring proper path configuration for Odoo addons.
    - Deleted the obsolete 'import2.vb' file, streamlining the project structure.
    
    These changes aim to improve the development environment setup and enhance clarity in configuration settings.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.380",
+    "version": "18.0.1.381",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
