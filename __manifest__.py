85f7fcb510f48b12827f7ffdec380fb5a9060dad
Ries Vriend <ries@vriend.com>
2025-01-25 14:01:52 +0100

    Add crew change radar functionality to log entries
    - Introduced a new computed field 'ship_crew_change_radar' to generate a descriptive link for crew change services
    - Added a new action method 'action_view_crew_change_radar' to display related services on a specific date
    - Updated log entry views to include a new button for accessing the crew change radar
    - Modified name computation logic to show start and end dates for back-office and end states
    - Added company reference to accounting configuration data products
    
    These changes enhance the visibility and tracking of crew changes and service-related information in the Rivermen module.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.384",
+    "version": "18.0.1.385",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
