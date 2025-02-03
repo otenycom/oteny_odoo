18c66451f0f46d52400d3e2a4070e1b1b0f47cb4
Thijs van Waaij <thijs@vriend.studio>
2025-02-03 12:50:48 +0100

    Add Odoo extension to VSCode configuration

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.395",
+    "version": "18.0.1.396",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
