4e1095b1427d05d369a1c59bf12903e7af76ad0b
Thijs van Waaij <thijs@vriend.studio>
2025-02-05 10:44:39 +0100

    Simplify RivermenRate Product Default Selection
    - Replace complex product search with direct reference to predefined 'Work Hours' product
    - Use `env.ref()` for more efficient and cleaner product default assignment
    - Streamline default product selection in RivermenRate model

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.402",
+    "version": "18.0.1.403",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
