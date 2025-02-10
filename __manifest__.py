01536d74d7ba56836b4264ca771e0622f40095a2
Ries Vriend <ries@vriend.com>
2025-02-10 17:07:36 +0100

    Update Billing Wizard to Use Service Deadline Instead of Supply Date
    - Modify passenger leg billing domain to use `service_id.deadline` instead of `supply_date`
    - Update travel values creation to reference `service_id.deadline` for supply date
    - Align with previous changes to remove `supply_date` from service model

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.418",
+    "version": "18.0.1.419",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
