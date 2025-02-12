b9faf2402a48ef2d9f4f054c138cc9bb0ba76ac4
Thijs van Waaij <thijs@vriend.studio>
2025-02-12 14:00:28 +0100

    Simplify Train Ticket Workflow by Removing Intermediate States
    - Remove 'Sent' state from train ticket workflow
    - Modify transition from 'Booked' state to directly register the service
    - Streamline workflow transitions to reduce unnecessary intermediate steps

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.423",
+    "version": "18.0.1.424",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
