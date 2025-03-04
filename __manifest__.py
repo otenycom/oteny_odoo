3608917c29870c0c2c9e58ab0683459f6b03b1a5
Thijs van Waaij <thijs@vriend.studio>
2025-03-04 15:20:47 +0100

    Add wizard for starting the next billing month
    Implemented a transient model wizard to facilitate the transition to the next billing month, including handling employee-related issues. Updated accounting configuration with a button and fields to trigger the wizard, along with necessary views and security rules.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.438",
+    "version": "18.0.1.439",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
