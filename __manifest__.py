10d0539c0cfda077623b2e68ba10bd9df7623233
Thijs van Waaij <thijs@vriend.studio>
2025-03-05 15:53:08 +0100

    Filter employee log entries by actual start and end date
    Updated the filtering logic to ensure log entries have an actual start date and end after October 1, 2024. This adds stricter validation to exclude outdated or incomplete entries.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.446",
+    "version": "18.0.1.447",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
