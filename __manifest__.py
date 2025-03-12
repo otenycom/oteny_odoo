227bd2ce620334326015de4de0aada6307df33e0
Thijs van Waaij <thijs@vriend.studio>
2025-03-12 10:19:43 +0100

    Updated Billing Wizard to handle having already billed all of the airfare contribution

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.461",
+    "version": "18.0.1.462",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
