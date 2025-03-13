418bc539310d98b6a65c4d7a0e5ab71faee491b2
Ries Vriend <ries@vriend.com>
2025-03-13 16:35:06 +0100

    Update Airfare Product Field in Accounting Config Views and Enhance Todo Documentation
    - Add `airfare_product_id` field to the `RivermenAccountingConfig` views to improve configuration options for airfare contributions.
    - Update the `riestodo.md` file to include additional testing scenarios for UI billing and airfare log entries, enhancing clarity on testing requirements and improving overall documentation.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.473",
+    "version": "18.0.1.474",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
