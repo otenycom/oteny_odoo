4ad516b0c2f305093f7fef7bd7cb6a91c11896b9
Ries Vriend <ries@vriend.com>
2025-03-16 13:26:18 +0100

    Add Odoo Test Method Launcher Extension to VSCode Configuration

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.474",
+    "version": "18.0.1.475",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
