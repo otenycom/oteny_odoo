5971949d76b5e80152217debdea570735efea4c2
Thijs van Waaij <thijs@vriend.studio>
2025-03-18 11:35:41 +0100

    Update Todo Documentation and Enhance Airfare Billing Wizard
    - Change section headers in `thijstodo.md` from H3 to H2 for improved readability and consistency.
    - Refactor the logic in `RivermenBillingWizardAirfare` to use the oldest entry date instead of the first entry where `did_fly_from_home` is True, enhancing accuracy in data processing.
    - Add a new form view for `airfare.contribution.billing.summary` in `rivermen_billing_wizard_views.xml`, providing a structured layout for billing summary details, including main information and contribution details.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.477",
+    "version": "18.0.1.478",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
