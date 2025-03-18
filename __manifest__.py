e44f7947c65b29b41dde97c55ea9c4ab1df38eab
Ries Vriend <ries@vriend.com>
2025-03-18 22:42:51 +0100

    Update Billing Logic and Documentation
    - Fix "Create Ignore Lines" function, set next billing date to the end of the following month.
    - Update the `LogEntryAirfare` model to handle cases where `expected_work_term_days` is zero, ensuring accurate calculations for billed airfare percentages.
    - Enhance the `_compute_expected_work_term_days` method in the `LogEntry` model to correctly derive values based on the presence of `tft_contract_id`, improving data accuracy.
    - Expand the todo documentation with new testing scenarios related to shadow run and end date imports, ensuring comprehensive tracking of responsibilities.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.480",
+    "version": "18.0.1.481",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
