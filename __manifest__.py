37f37e8bd2adfadc9c6f4011680033a920f11082
Ries Vriend <ries@vriend.com>
2025-03-19 14:56:38 +0100

    Add Check Result Integration to Rivermen Ship Model and Update Billing Logic
    - Introduced `check_result_ids` and `billing_check_result_ids` fields in the `RivermenShip` model to track issues and billing-related checks, enhancing data management.
    - Implemented `_compute_check_results` and `_compute_billing_check_results` methods to dynamically compute and filter check results based on defined criteria.
    - Updated the `EntryBillingLine` model to include a filter for `is_actual` in the domain, ensuring only relevant entries are processed.
    - Enhanced the billing wizard to include checks for missing work terms, providing better visibility of issues during billing processes.
    - Made minor adjustments to the XML views to accommodate new fields and improve user interface clarity.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.482",
+    "version": "18.0.1.483",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
