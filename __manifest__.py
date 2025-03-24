fc1db25c8c2af74c80ae9d7de3b37be85f252e5c
Thijs van Waaij <thijs@vriend.studio>
2025-03-24 12:57:44 +0100

    Enhance Excel Import Functionality by Adding New Import Modules
    - Introduced new import functions for companies, employees, log entries, log entry planning, partner clients, and river credentials, expanding the capabilities of the `ImportExcel` command.
    - Updated the `OdooClient` class to include a caching mechanism for XML IDs, improving performance during record creation and retrieval.
    - Refactored existing import logic to streamline the process and ensure clarity in handling various document types.
    - Enhanced the VSCode launch configuration to support dynamic import paths, facilitating easier testing and development.
    - Removed redundant external ID lookups in the `import_ships` and `import_time_for_time_contracts` functions, optimizing the import process.
    - Added detailed logging and error handling to improve traceability and debugging during the import operations.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.489",
+    "version": "18.0.1.490",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
