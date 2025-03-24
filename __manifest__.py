843db72a3dc28146a1c92f8f97a3e4aeebe9a0d9
Ries Vriend <ries@vriend.com>
2025-03-24 14:58:31 +0100

    Update .gitignore and Refactor Launch Configuration and Model Code
    - Added `sqldumps/` to `.gitignore` to prevent SQL dump files from being tracked.
    - Updated the VSCode `launch.json` to specify the path for the Excel import file, enhancing clarity for import operations.
    - Refactored the `rivercreds_credential.py` model by commenting out unused mixins to improve code clarity and maintainability.
    - Cleaned up the `riverflow_check_result.py` model by commenting out the `_compute_state_record_id` method, which is no longer in use.
    - Streamlined field definitions in `rivercreds_credential.py` for better readability.
    - Made minor adjustments to XML views in `rivercreds_credential_views.xml` to comment out unused fields and improve the user interface.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.490",
+    "version": "18.0.1.491",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
