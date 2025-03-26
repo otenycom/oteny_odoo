e2cc7fb1afa2ea8b1e78fac27c0d2cd763644489
Thijs van Waaij <thijs@vriend.studio>
2025-03-26 12:27:29 +0100

    Add Employee Import Target to Excel Import Command
    - Updated the `ImportExcel` command to include "employees_inhouse" in the list of import targets under "users_companies", allowing for the import of in-house employee data alongside user data from Excel files.
    - This addition aims to enhance the flexibility of the import process by enabling simultaneous handling of user and employee data, streamlining overall data management during imports.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.504",
+    "version": "18.0.1.505",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
