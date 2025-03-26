a3f6ce926cd684dd54fbbcf207cc716a47d223d0
Thijs van Waaij <thijs@vriend.studio>
2025-03-26 16:10:18 +0100

    Update Import Functionality and Logging in Excel Import
    - Changed the import target in the `launch.json` from "users_companies" to "all" to facilitate broader import capabilities.
    - Commented out the "post_import" target in `importexcel.py` to prevent its execution during the import process, ensuring a cleaner import workflow.
    - Enhanced the logging mechanism in the `Import` model to provide clearer notifications regarding the handling of planning entries and duplicates, improving traceability and clarity in the import process.
    
    These changes aim to streamline the import functionality and improve the overall user experience during data imports.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.507",
+    "version": "18.0.1.508",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
