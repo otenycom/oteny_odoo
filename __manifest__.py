cbcf1c58659525db4fb25f8cd1b14090876337cc
Thijs van Waaij <thijs@vriend.studio>
2025-03-26 12:25:38 +0100

    Enhance User Import Functionality in Excel Import
    - Updated the `ImportExcel` command to include a new import target "users_companies", allowing for the simultaneous import of users and companies from Excel files.
    - Modified the `import_users` function in `res_users.py` to handle an additional "groups" column, enabling the assignment of user groups during the import process.
    - Implemented logic to retrieve company IDs and associate them with users, improving the user import process by linking users to their respective companies.
    - Adjusted the handling of passwords to use "new_password" for clarity and updated comments to reflect the new functionality.
    - These changes aim to enhance the flexibility and efficiency of user data management through Excel imports, streamlining the overall import workflow.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.503",
+    "version": "18.0.1.504",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
