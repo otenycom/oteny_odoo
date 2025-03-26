f797402e41d597e28b21868a51aafe831363eb0a
Thijs van Waaij <thijs@vriend.studio>
2025-03-26 10:55:15 +0100

    Update Excel Import Paths and Enhance Duplicate Entry Logging
    - Modified the paths for importing company and employee data in the `ImportExcel` command to remove the "odoo/" prefix, simplifying the file structure.
    - Enhanced the logging mechanism for duplicate entries in the `Import` model by including the external ID in the notification message, improving traceability of duplicate records.
    - These changes aim to streamline the import process and provide clearer feedback on duplicate handling during data imports.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.499",
+    "version": "18.0.1.500",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
