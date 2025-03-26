fa5e6efc84fe78a0e85f8673a61b81b5b3e6ff8f
Thijs van Waaij <thijs@vriend.studio>
2025-03-26 16:12:30 +0100

    Add Airfare Contribution Lines Page to Billing Wizard
    - Introduced a new page for "Airfare Contribution Lines" in the billing wizard, allowing users to manage airfare contribution line items effectively.
    - The page includes a list view with fields for product, log entry, ship, employee, issues summary, company, quantity days, price unit, and additional details, enhancing data visibility and management.
    - Removed the previous duplicate page for "Airfare Contribution Lines" to maintain clarity and avoid redundancy in the user interface.
    
    These changes aim to improve the billing wizard's functionality and user experience by providing a dedicated section for airfare contributions.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.508",
+    "version": "18.0.1.509",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
