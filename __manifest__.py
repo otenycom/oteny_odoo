f68cf4686c16def67f264f49d47764b350f73b90
Thijs van Waaij <thijs@vriend.studio>
2025-03-26 11:05:25 +0100

    Update Access Rights in ir.model.access.csv
    - Retained existing access rights for the `riverflow.access_riverflow_enter_train_ticket_wizard` model, ensuring no disruption to current permissions.
    - Added access rights for the `rivermen.access_rivermen_import` model, allowing users in the `base.group_user` group to access the import functionality.
    - These changes aim to enhance user access management for the import feature while maintaining existing functionality.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.501",
+    "version": "18.0.1.502",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
