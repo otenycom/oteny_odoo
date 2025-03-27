6b69229de45127f2052cc3793b4717c7f82f7c8b
Thijs van Waaij <thijs@vriend.studio>
2025-03-27 14:34:34 +0100

    Update Airfare Rate Import and Log Entry Logic
    - Marked the "Airfare rate import" task as completed in the `riestodo.md` file, indicating progress in the project.
    - Enhanced the logic in the `LogEntryAirfare` model to account for additional conditions when determining the previous remaining airfare percentage, improving the accuracy of calculations based on work status and workflow state.
    
    These changes aim to refine the airfare rate import process and ensure more precise handling of airfare contributions in the application.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.513",
+    "version": "18.0.1.514",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
