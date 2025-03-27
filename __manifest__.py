b0acf3f8c114f83df1cba10fcf319de2a6c10594
Ries Vriend <ries@vriend.com>
2025-03-27 16:44:27 +0100

    Enhance Check Result Logic and Add New Check Type
    - Introduced a new check type "prior_entry_has_missing_who_pays_remaining_airfare_contribution" in the `CheckResult` model to handle specific validation scenarios related to airfare contributions.
    - Updated the comparison logic in the `CheckResult` model to include the new check type, ensuring comprehensive validation.
    - Modified the `LogEntryAirfare` model to incorporate checks for the new condition, enhancing the accuracy of error reporting for entries lacking 'Who Pays' information.
    - Added a test case to verify the behavior of the new check type, ensuring that the system correctly identifies and reports missing 'Who Pays' contributions.
    
    These changes aim to improve the validation process for airfare contributions and enhance the overall integrity of the billing workflow.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.514",
+    "version": "18.0.1.515",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
