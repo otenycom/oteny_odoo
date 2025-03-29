0659b764d3b01c671a4341064aa33be1d9ec827f
Ries Vriend <ries@vriend.com>
2025-03-29 15:48:30 +0100

    fix intellisense/pyright config

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.521",
+    "version": "18.0.1.522",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
