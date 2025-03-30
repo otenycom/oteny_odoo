b188f1777131328c8ce6605b35d555352940dd97
Ries Vriend <ries@vriend.com>
2025-03-30 16:14:20 +0200

    supply_unit_price in services list for reporting airline costs per month

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.532",
+    "version": "18.0.1.533",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
