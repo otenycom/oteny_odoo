b2051da24ec2fbe740f6297760b7a998c93b6e05
Thijs van Waaij <thijs@vriend.studio>
2025-03-31 14:52:31 +0200

    Refactor billing wizard structure and update imports
    - Moved billing wizard views to a new directory structure for better organization.
    - Removed obsolete mixin and billing wizard airfare files to streamline the codebase.
    - Updated the __init__.py file to reflect the new import paths for the billing wizard.
    - Adjusted the __manifest__.py to include the new path for billing wizard views.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.534",
+    "version": "18.0.1.535",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
