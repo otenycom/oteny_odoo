d3d5d0dfe4e0cb4b7c614e4e69e747ceec8d9815
Thijs van Waaij <thijs@vriend.studio>
2025-04-01 10:26:01 +0200

    Refactor billing summary models and views for improved structure
    - Introduced a new base model `rivermen.billing.summary.base` to unify billing summary handling across work, travel, and airfare contributions.
    - Updated related wizards to reference the new base model, ensuring consistent data handling.
    - Added a method to dynamically open the correct view based on the product type in the billing summary base.
    - Adjusted XML views to reflect the new model structure and removed obsolete records.
    - Enhanced the billing wizard to include a combined list of all summary lines for better visibility and management.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.535",
+    "version": "18.0.1.536",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
