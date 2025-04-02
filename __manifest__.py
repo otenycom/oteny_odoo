593795d93fa6a14b6a6345bb1f681738b17af91a
Thijs van Waaij <thijs@vriend.studio>
2025-04-02 16:02:27 +0200

    Enhance billing wizard functionality by adding summary line computation
    - Added `partner_id` to airfare contribution values in `RivermenBillingWizardAirfare` for improved operator association.
    - Introduced `is_summary_lines_computed` field in `RivermenBillingWizard` to track summary line computation status.
    - Updated onchange methods to reset `is_summary_lines_computed` when relevant fields change, ensuring accurate summary line calculations.
    - Enhanced `_compute_summary_lines` method to skip computation if summary lines have already been computed, improving performance.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.544",
+    "version": "18.0.1.545",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
