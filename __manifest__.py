017ba71f8417bf6721fabe3aed22defa8a37631f
Thijs van Waaij <thijs@vriend.studio>
2025-04-02 13:26:48 +0200

    Enhance RiverDeploy script with new options and improved shell readiness checks
    - Added a new OPTIONS dictionary to define various deployment paths for different environments, enhancing flexibility in deployment operations.
    - Updated the Odoo server context manager to accept a tmux pane parameter, allowing for more precise control over server operations.
    - Improved the shell readiness check to accommodate both local and SSH sessions, ensuring reliable command execution in diverse environments.
    - Refactored the rsync command to target the correct directory for backup files, streamlining the deployment process.
    - Removed outdated signal handling code and ensured graceful shutdown of the tmux session during interruptions.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.543",
+    "version": "18.0.1.541",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
