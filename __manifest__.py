b3d8a524d68ec8351c44c078fb5e2b33647374af
Thijs van Waaij <thijs@vriend.studio>
2025-04-07 14:49:05 +0200

    Enhance TMux session management by enabling mouse mode
    - Updated the `create_or_connect_session` method in the `TMuxManager` class to enable mouse mode for easier navigation within TMux sessions. This improvement enhances user experience by allowing intuitive mouse interactions.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.553",
+    "version": "18.0.1.554",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
