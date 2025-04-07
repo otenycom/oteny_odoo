e842b5edc22bef147d1ecdb518005a0870493bd5
Thijs van Waaij <thijs@vriend.studio>
2025-04-07 13:56:48 +0200

    Enhance travel expense import functionality and deduplication logic
    - Added a new section in the documentation for post-import processes, detailing the deduplication of log entries based on the most recent actual log entry and matching PLAN_ records.
    - Implemented deduplication logic in the `Import` model to identify and handle duplicate log entries based on employee and ship criteria, with a tolerance margin for start and end dates.
    - Improved the handling of message associations during deduplication to ensure relevant messages are transferred to the correct entries.
    - Ensured that duplicate entries are properly unlinked after processing, maintaining data integrity.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.548",
+    "version": "18.0.1.549",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
