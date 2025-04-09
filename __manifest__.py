55814e3829095bdd18f9b89aa7214a8f05ff00b1
Thijs van Waaij <thijs@vriend.studio>
2025-04-09 13:34:38 +0200

    Added title to changelog

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.561",
+    "version": "18.0.1.562",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
