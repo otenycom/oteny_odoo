81d805d40b86686c73074351dc1809ebff88e903
Ries Vriend <ries@vriend.com>
2025-04-11 17:47:58 +0200

    generated entries readonly

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.567",
+    "version": "18.0.1.568",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
