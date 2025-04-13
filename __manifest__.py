6a487958d962d65ff6a98414cecfc308ce080a45
Thijs van Waaij <thijs@vriend.studio>
2025-04-14 00:30:22 +0200

    Update README to clarify function execution method
    - Revised the section on running functions to specify that execution can be done via F5 or other shortcuts, enhancing clarity for users on how to utilize the deployment tool effectively.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.560",
+    "version": "18.0.1.561",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
