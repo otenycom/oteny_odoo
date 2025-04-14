d1bea6a278fd38cc9da886e731837f71fd6e4389
Thijs van Waaij <thijs@vriend.studio>
2025-04-14 12:07:08 +0200

    Add vacation log entry model and update views
    - Introduced a new model `LogEntryVacation` to handle vacation-related log entries, including fields for vacation differential hours and balances.
    - Updated the `__init__.py` to include the new model.
    - Added a new XML view for displaying vacation balance information in the employee form.
    - Modified existing log entry views to incorporate vacation balance fields, enhancing the visibility of vacation data.
    - Removed redundant vacation fields from the original log entry model to streamline the codebase.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.564",
+    "version": "18.0.1.566",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
