d2408d0fc5f14a45566720643200b0ca4ce7b8eb
Ries Vriend <ries@vriend.com>
2025-04-15 15:43:49 +0200

    vacation_balance_hours_at_start

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.578",
+    "version": "18.0.1.579",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
