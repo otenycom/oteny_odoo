7a5ae2e14406c34236cb34479ce63c8201a5fde7
Ries Vriend <ries@vriend.com>
2025-04-15 16:23:03 +0200

    adjustment vba

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.580",
+    "version": "18.0.1.581",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
