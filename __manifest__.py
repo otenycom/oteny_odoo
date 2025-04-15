567aa64e358227bfa75d27f78d81c02ef7c6328a
Ries Vriend <ries@vriend.com>
2025-04-15 14:07:00 +0200

    fix indonesia

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.577",
+    "version": "18.0.1.578",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
