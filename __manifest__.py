c0da20dc21f2fc9d21f3546b458b0203833aec2b
Ries Vriend <ries@vriend.com>
2025-04-16 15:53:36 +0200

    master record view

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.590",
+    "version": "18.0.1.591",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
