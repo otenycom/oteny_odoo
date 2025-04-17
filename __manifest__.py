4e455997d00d361c489a432b97fc8e7e536de8cd
Thijs van Waaij <thijs@vriend.studio>
2025-04-17 10:45:54 +0200

    Basic tft invoicing

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.594",
+    "version": "18.0.1.595",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
