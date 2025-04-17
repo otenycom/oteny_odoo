b0165973ac16c5a34a5e7d7c78958c54262d2f17
Thijs van Waaij <thijs@vriend.studio>
2025-04-17 10:52:51 +0200

    Fixed manual shipterm and  tft now only gets billed when percentage 50% or more

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.595",
+    "version": "18.0.1.596",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
