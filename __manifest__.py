f7ab6dd883253d80222901455f060951377df430
Ries Vriend <ries@vriend.com>
2025-04-17 16:32:13 +0200

    fix gap in old tester

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.597",
+    "version": "18.0.1.598",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
