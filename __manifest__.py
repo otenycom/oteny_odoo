678a724c3c04dfee38ec410445b90eab21d445d6
Thijs van Waaij <thijs@vriend.studio>
2025-04-21 10:23:18 +0200

    Add odoo_settings.xml to set English (US) week start to Monday and update todo list formatting

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.598",
+    "version": "18.0.1.599",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
