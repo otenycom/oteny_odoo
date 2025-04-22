35b848b5f37f8a8067f8519c528cf7551ab83f9b
Thijs van Waaij <thijs@vriend.studio>
2025-04-22 11:15:06 +0200

    Removed odoo_settings.xml as it is no longer needed. Updated week start for English (US) language to Monday in the hooks.py file.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.600",
+    "version": "18.0.1.601",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
