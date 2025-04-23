d8aec843629e94089794d019a03db0be2bcaa7b5
Thijs van Waaij <thijs@vriend.studio>
2025-04-23 14:26:47 +0200

    Added sections of employees on the final invoice

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.604",
+    "version": "18.0.1.605",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
