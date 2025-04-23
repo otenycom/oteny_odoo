8bab40d8bc577a156e6376665beb2285ad806401
Thijs van Waaij <thijs@vriend.studio>
2025-04-23 15:44:37 +0200

    Added a note to the TODO list for the billing wizard to include standby away in invoice lines.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.607",
+    "version": "18.0.1.608",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
