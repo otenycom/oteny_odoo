ba22671cc9b78d32c0d55a94ffeef6530c345ccf
Thijs van Waaij <thijs@vriend.studio>
2025-04-28 16:33:07 +0200

    Add timesheet functionality including models, views, and access rights.
    - Introduced `rivermen.timesheet` and `rivermen.timesheet.log.entry` models to manage timesheet entries and their associated log entries.
    - Created XML views for timesheet forms, lists, and search functionality.
    - Updated `__manifest__.py` to include new views and added access rights for the timesheet models in `ir.model.access.csv`.
    - Added a new group for Timesheet Manager in security XML.
    - Included placeholder for future functionality in the timesheet form view.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.618",
+    "version": "18.0.1.619",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
