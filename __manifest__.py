f40073cc4e8453ddb078aff5f914513e357debdc
Thijs van Waaij <thijs@vriend.studio>
2025-04-28 10:21:27 +0200

    Updated CHANGELOG to remove redundant wording in the vacation balance Accounting Entry report entry. Adjusted notes in riestodo and thijstodo markdown files for clarity and completion status.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.613",
+    "version": "18.0.1.614",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
