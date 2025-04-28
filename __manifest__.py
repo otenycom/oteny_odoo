c9361d1f959514713b48bf133a7b4e6c768fffc7
Thijs van Waaij <thijs@vriend.studio>
2025-04-28 15:05:17 +0200

    x

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.617",
+    "version": "18.0.1.618",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
