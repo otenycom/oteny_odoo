73cf07ef22bb98c6356b6a7947a335138e067812
Thijs van Waaij <thijs@vriend.studio>
2025-04-30 11:35:00 +0200

    Timesheet imports

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.626",
+    "version": "18.0.1.627",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
