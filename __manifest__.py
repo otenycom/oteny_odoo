85c4aaee93826274514c19da5bb0b83fbf7ae2ea
Thijs van Waaij <thijs@vriend.studio>
2025-04-30 11:12:57 +0200

    Changes while in call

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.625",
+    "version": "18.0.1.626",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
