1069bc817e7944fc21b3b6132074af761a877c48
Ries Vriend <ries@vriend.com>
2025-05-01 16:25:28 +0200

    timesheet importer icons etc

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.633",
+    "version": "18.0.1.634",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
