a67658f3808735a2a650959c3fe1e2b8047297f1
Ries Vriend <ries@vriend.com>
2025-05-02 10:02:35 +0200

    rename importer files

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.634",
+    "version": "18.0.1.635",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
