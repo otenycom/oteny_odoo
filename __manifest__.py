1098b8c9664d3fdc0704ee2642f30f866ae088c2
Thijs van Waaij <thijs@vriend.studio>
2025-05-06 10:21:55 +0200

    Moved from a wizard to an valuation line as a model. As ries requested

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.636",
+    "version": "18.0.1.637",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
