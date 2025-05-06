78c2b638eb44b8f55a045264ff4377c89fd20315
Thijs van Waaij <thijs@vriend.studio>
2025-05-06 12:17:53 +0200

    Update Servers.json

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.639",
+    "version": "18.0.1.640",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
