dfb0edf23cd2d447cf30e236c774e8dd32060061
Ries Vriend <ries@vriend.com>
2025-05-09 23:33:47 +0200

    fix testers / remove old work workflow

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.658",
+    "version": "18.0.1.659",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
