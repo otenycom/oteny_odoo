ffdea8247c2260c990c9bc027ebfa4871653aa66
Ries Vriend <ries@vriend.com>
2025-05-09 11:53:42 +0200

    merge todo lists

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.652",
+    "version": "18.0.1.653",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
