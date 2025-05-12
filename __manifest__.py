c240a0bfd7f89ca2dedd235cd5238b7daedb3f2b
Thijs van Waaij <thijs@vriend.studio>
2025-05-12 15:23:35 +0200

    Update vacation scheme handling and related models
    - Added position_id field to the default vacation scheme model to allow differentiation of vacation schemes based on employee position.
    - Updated the vacation scheme retrieval logic in LogEntryVacation to utilize the new find_scheme method, improving clarity and maintainability.
    - Adjusted the sorting order of vacation schemes to prioritize specificity.
    - Enhanced vacation balance report to include log entry references for better tracking.
    - Added tests to validate vacation scheme selection based on employee position.
    
    This commit improves the vacation management functionality by ensuring that vacation schemes can be tailored to specific employee roles, enhancing the overall usability of the system.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.669",
+    "version": "18.0.1.670",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
