e2ed54c47768c20977e856dd6db51b0a427a8024
Thijs van Waaij <thijs@vriend.studio>
2025-05-13 13:22:58 +0200

    Implemented tests

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.675",
+    "version": "18.0.1.676",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
