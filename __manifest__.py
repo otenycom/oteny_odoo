9c48f60873c05a9c50333bfc27a854db49707a75
Thijs van Waaij <thijs@vriend.studio>
2025-05-13 15:52:04 +0200

    Vacation hours report on vacation entry test

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.677",
+    "version": "18.0.1.678",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
