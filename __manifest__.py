3f434f29f389a5bc05a6033b7ccbd9e104146b58
Ries Vriend <ries@vriend.com>
2025-05-13 17:52:34 +0200

    fix to be planned generated entries + testers

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.678",
+    "version": "18.0.1.679",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
