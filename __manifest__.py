dcd1ad2c1e1fd681a5870ece838456fb1ace2045
Ries Vriend <ries@vriend.com>
2025-05-13 08:18:37 +0200

    test outline

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.673",
+    "version": "18.0.1.674",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
