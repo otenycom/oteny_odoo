3954d15b89cc2094094998722d563d9ae1103970
Thijs van Waaij <thijs@vriend.studio>
2025-05-14 11:55:09 +0200

    Added periods to vacation calculation

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.682",
+    "version": "18.0.1.683",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
