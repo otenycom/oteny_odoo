06e423df95c67b506c879fde3c1069fd1e5c049e
Thijs van Waaij <thijs@vriend.studio>
2025-05-18 20:17:29 +0200

    Fixed test has_been_invoiced

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.688",
+    "version": "18.0.1.689",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
