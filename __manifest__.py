7301d848dc9fe3ae6893a9f037236c9e6cd46607
Thijs van Waaij <thijs@vriend.studio>
2025-05-19 10:45:48 +0200

    fix for error on import

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.690",
+    "version": "18.0.1.691",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
