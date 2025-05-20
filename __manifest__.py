05b6f1aa751b6621bd4ef5204bf8d80607b30343
Thijs van Waaij <thijs@vriend.studio>
2025-05-20 09:41:28 +0200

    When no accounting config is found return early

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.695",
+    "version": "18.0.1.696",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
