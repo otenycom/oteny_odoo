e8830381b62f3860b2cc428fb68aad48af5b0b88
Thijs van Waaij <thijs@vriend.studio>
2025-05-20 09:33:24 +0200

    Overtime now also shows when its 0

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.693",
+    "version": "18.0.1.695",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
