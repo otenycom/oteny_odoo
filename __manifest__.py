3a9511a444ab36be0db13a6250228cd4a9956f6e
Thijs van Waaij <thijs@vriend.studio>
2025-05-20 10:34:53 +0200

    Added back duplicates check for timesheet and import now everything. And fixed the len of bool is invalid error

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.696",
+    "version": "18.0.1.697",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
