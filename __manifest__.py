a6e69acf1d5bf3a5414b906b080aa3994788d516
Ries Vriend <ries@vriend.com>
2025-05-22 19:17:06 +0200

    PNR sequence plus diplicate check, prefix rivermen_ for hr_employee fields

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.704",
+    "version": "18.0.1.705",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
