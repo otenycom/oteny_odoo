8cc810f1278044ebae3f14661d6ead6eea9b3b3e
Ries Vriend <ries@vriend.com>
2025-05-22 21:54:50 +0200

    ship, client import, pnr sequence in odoo

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.705",
+    "version": "18.0.1.706",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
