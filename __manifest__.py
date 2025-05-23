ec0e4594ecd0e510803ff17b0c0422f3dccbaf67
Ries Vriend <ries@vriend.com>
2025-05-23 20:16:45 +0200

    position, finance/bank account import to excel

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.711",
+    "version": "18.0.1.712",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
