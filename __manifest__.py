6a89826f3fe9ae0eb21bacb73eb5526512ae85d7
Ries Vriend <ries@vriend.com>
2025-05-23 15:53:24 +0200

    SV/Tax fields import from Odoo

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.708",
+    "version": "18.0.1.709",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
