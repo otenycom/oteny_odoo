a975c341a4abf0319e74af050cf4aea1eee586e7
Ries Vriend <ries@vriend.com>
2025-05-23 15:28:22 +0200

    import tax + sv

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.707",
+    "version": "18.0.1.708",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
