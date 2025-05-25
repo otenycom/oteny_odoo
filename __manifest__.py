d326d083f7255c6601e46f0a60aa9398114b3ca3
Ries Vriend <ries@vriend.com>
2025-05-25 09:14:01 +0200

    log entries to excel

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.717",
+    "version": "18.0.1.718",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
