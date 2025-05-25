8633af0754c9b801f4ddb396f6bf73d743021e2d
Ries Vriend <ries@vriend.com>
2025-05-25 12:28:48 +0200

    fix 5 second time out in deploy debug

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.719",
+    "version": "18.0.1.720",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
