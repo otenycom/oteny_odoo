a149950c594abbad10124f6a4d22c272b1f0b32a
Ries Vriend <ries@vriend.com>
2025-05-25 13:48:36 +0200

    Post-import merge: always keep the actual entry, so we always keep CUNREFS the same and don't switch to a generated sequence number from the plan record

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.721",
+    "version": "18.0.1.722",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
