6d64d649c1ad9cdbeba5c9cc3f7c9e5e770e9ebf
Ries Vriend <ries@vriend.com>
2025-05-25 11:55:00 +0200

    timesheet import & deploy port 8070 to isolate it from dev

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.718",
+    "version": "18.0.1.719",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
