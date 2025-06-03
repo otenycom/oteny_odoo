b2b235958dc5deb66219322c86fd8632d65b3fd1
Ries Vriend <ries@vriend.com>
2025-06-03 15:46:42 +0200

    planning cunref

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.738",
+    "version": "18.0.1.739",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
