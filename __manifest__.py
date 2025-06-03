0bcc1d9b6376cc0c4959af523659b6fa337a909f
Ries Vriend <ries@vriend.com>
2025-06-03 14:48:44 +0200

    sequence fix

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.735",
+    "version": "18.0.1.736",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
