62860ee62692ff4ea142fba78f9e3694dad0747e
Ries Vriend <ries@vriend.com>
2025-06-03 15:36:52 +0200

    teams fix cunref fix

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.737",
+    "version": "18.0.1.738",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
