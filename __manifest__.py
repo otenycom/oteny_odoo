cbec48757b345cebfc5f59e6448bbef5edffb549
Thijs van Waaij <thijs@vriend.studio>
2025-06-04 11:11:36 +0200

    Added travel rate to import

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.741",
+    "version": "18.0.1.742",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
