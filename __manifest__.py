5a29ade15d3e532075f7331ac285c6b859744b48
Thijs van Waaij <thijs@vriend.studio>
2025-06-05 11:33:43 +0200

    Fix vacation balance calculator, fix log_entry readonly on generated

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.742",
+    "version": "18.0.1.743",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
