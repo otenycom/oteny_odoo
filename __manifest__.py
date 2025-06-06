099b0ac5493ba3bae8afadb82af7e23c229ceeb9
Ries Vriend <ries@vriend.com>
2025-06-06 22:04:01 +0200

    [x] Radar: Entry issues don't show reliably

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.749",
+    "version": "18.0.1.750",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
