468f5b65c2a7561fdbfa59daca1f5c87ea1341a2
Ries Vriend <ries@vriend.com>
2025-06-06 14:41:45 +0200

    - [x] Log Entry List Sort order by default: deadline, name   - [x] Log Entry list: add filter for Today + Upcoming   - [x] Allow setting company on entry if employee is not yet known, for plannig purposes   - [x] Add short name field to each company (DU, CS, CNL)   - [x] Add company short name to log entry name   - [x] Add company short name/employee nationality/position to log entry form   - [x] Employee nationality in Log Entry Form

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.746",
+    "version": "18.0.1.747",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
