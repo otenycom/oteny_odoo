2b37d47f361293c73081f0d9aaffddf4638c9131
Ries Vriend <ries@vriend.com>
2025-06-06 14:49:28 +0200

    remove operator from ship displayname

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.747",
+    "version": "18.0.1.748",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
