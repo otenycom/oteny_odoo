2965da9acf254dc4828db1ac471ef106c8efe28a
Ries Vriend <ries@vriend.com>
2025-06-07 22:39:24 +0200

    - [x] CS employees uit planmap op 'actual start' zetten op basis van startdatum     - [x] werkt niet voor Oclarit, Prosniewski? (date conversion string issue)

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.753",
+    "version": "18.0.1.754",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
