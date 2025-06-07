9d033f4bb8ecf2f3b6423a5cc38e126f0a279ab9
Ries Vriend <ries@vriend.com>
2025-06-07 21:03:59 +0200

    - Planning/UI features   - [x] Add company short name to logbook list   - [x] Gantt chart popup template met vacation balance info

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.752",
+    "version": "18.0.1.753",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
