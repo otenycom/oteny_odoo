681309bd16c597884b3d23e2601f716e34075da2
Ries Vriend <ries@vriend.com>
2025-06-07 17:39:50 +0200

    - [x] Review column index voor tags, entry list vs service consistennt maken

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.751",
+    "version": "18.0.1.752",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
