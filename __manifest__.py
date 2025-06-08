090bf236126b2ad9c34025106532c4fb4906248b
Ries Vriend <ries@vriend.com>
2025-06-08 13:07:08 +0200

    - [x] CS Staff should not be included in employee checks (needs passport etc).           Need a accounting_config flag, employee flag, needs_credential_checks or is_freelance o.i.d     - [x] Skip is_airfare_contribution_applicable, gaps, for CS

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.755",
+    "version": "18.0.1.756",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
