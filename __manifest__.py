0c7e7428b709ceb02a00a0084c42144f80ac96e9
Ries Vriend <ries@vriend.com>
2025-06-08 13:36:51 +0200

    - [x] Color of 'prep sign on' light blue, colors plan (orange) -> Arrange Sign On (light blue) -> Signed on (blue)

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.756",
+    "version": "18.0.1.757",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
