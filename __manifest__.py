78c939604d2f9a77e79d516109abbf9e65dbb1f8
Thijs van Waaij <thijs@vriend.studio>
2025-06-11 12:39:03 +0200

    cash hours payout todo

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.764",
+    "version": "18.0.1.765",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
