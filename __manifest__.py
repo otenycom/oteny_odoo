ecb27380c90fcdc2cecfe09b32a65d4d2448164e
Ries Vriend <ries@vriend.com>
2025-06-11 21:16:27 +0200

    [x] Redirect naar de supply order service vanuit radar list click op de info service

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.770",
+    "version": "18.0.1.771",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
