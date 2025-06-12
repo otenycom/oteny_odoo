d1f9bdc686fa9e50031356216e0c59dfd450284e
Ries Vriend <ries@vriend.com>
2025-06-13 01:11:49 +0200

    - [~] !Allow drag/drop (sub-sequence) for top-level services, allows multi modal travel services to be placed in the correct order     - [x] Basic drag drop + drag handle now works in services tab of entry and service child service lists     - [ ] bug: dragging child services to the top most position fails and assign lasts position to the dropped child service

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.779",
+    "version": "18.0.1.780",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
