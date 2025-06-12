fa09d1d321507fe42bb566c258f0b176c9724583
Ries Vriend <ries@vriend.com>
2025-06-13 01:03:26 +0200

    fix drag handle icon (hack)

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.778",
+    "version": "18.0.1.779",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
