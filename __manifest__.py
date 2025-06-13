9f7ae885da74a34296f921d25639d328d4b84435
Ries Vriend <ries@vriend.com>
2025-06-13 16:47:58 +0200

    todo

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.780",
+    "version": "18.0.1.781",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
