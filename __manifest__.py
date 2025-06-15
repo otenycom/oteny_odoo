e80b27c000525165afecba6363f25e20103857e5
Ries Vriend <ries@vriend.com>
2025-06-15 16:21:59 +0200

    todo omschreven voor logbook calendar

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.785",
+    "version": "18.0.1.786",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
