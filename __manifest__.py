10ba01f7c54b7839729b3fe422a911e87d2951a5
Thijs van Waaij <thijs@vriend.studio>
2025-06-18 14:25:08 +0200

    Other charge services

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.789",
+    "version": "18.0.1.792",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
