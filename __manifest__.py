e426c457708fc7d67b6207355bf40888087aea7a
Ries Vriend <ries@vriend.com>
2025-06-21 17:13:13 +0200

    - [x] Voor operations moet 'Sick' Planned mogelijk zijn "vooruit planning" van ziekte, bv een operatie bij de dokter volgnede week.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.795",
+    "version": "18.0.1.796",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
