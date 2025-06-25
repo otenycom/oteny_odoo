11c29021d773e8106a149bccc01c3179a7373e1d
Thijs van Waaij <thijs@vriend.studio>
2025-06-25 14:15:34 +0200

    Changed data structure for others

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.801",
+    "version": "18.0.1.802",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
