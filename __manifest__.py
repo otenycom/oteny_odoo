b0a15ee956dffbcebf10cdfe3cf22566459379ee
Thijs van Waaij <thijs@vriend.studio>
2025-06-26 09:40:42 +0200

    delete test_invoice_credit_note

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.803",
+    "version": "18.0.1.804",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
