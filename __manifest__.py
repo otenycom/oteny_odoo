7006e124344f6f528757d5594ff712f8d88dfe5a
Ries Vriend <ries@vriend.com>
2025-06-26 16:54:15 +0200

    - [x] Cash entries workflow weg, nu gewoon Vacation Payouts tabel geworden

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.811",
+    "version": "18.0.1.812",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
