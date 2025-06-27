06340ba51947b41bf2317f0a9635bf0eb5b6c349
Ries Vriend <ries@vriend.com>
2025-06-27 17:34:26 +0200

    set up https://rivermen.cuneuscrew.eu

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.813",
+    "version": "18.0.1.814",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
