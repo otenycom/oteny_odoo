cf74bbdabc177b5b07c3c3becf036678e4ba9123
Ries Vriend <ries@vriend.com>
2025-06-27 17:22:26 +0200

    review other charges

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.812",
+    "version": "18.0.1.813",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
