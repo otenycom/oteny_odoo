a68ed9c33c3c3da8f4ec898d83da94785c02adb8
Thijs van Waaij <thijs@vriend.studio>
2025-07-01 10:15:01 +0200

    Fixed issue with date on the balance report

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.817",
+    "version": "18.0.1.818",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
