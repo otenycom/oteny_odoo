05e1f3798056123efadb8cdbf918202e7f9b6833
Ries Vriend <ries@vriend.com>
2025-08-19 12:36:47 +0200

    a1 and mfnl workflows

--- __manifest__.py
+++ __manifest__.py
@@ -33,6 +33,7 @@
         "data/transition_action_data.xml",
         "data/workflow_service_task.xml",
         "data/service_email_workflow.xml",
+        "data/service_arrange_and_send_workflow.xml",
         # "data/riverflow_back_office_workflow.xml",
         "data/riverflow_taxi_order_workflow.xml",
         "data/riverflow_to_be_invoiced_workflow.xml",
