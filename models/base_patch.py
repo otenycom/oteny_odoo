a66a42ad854a949cd3a6d2d04ec314daf62a5eb8
Thijs van Waaij <thijs@vriend.studio>
2025-08-25 14:25:53 +0200

    fixed warning in tests

--- models/base_patch.py
+++ models/base_patch.py
@@ -70,7 +70,7 @@ def patched_create(self, vals_list):
             for col in columns:
                 field = model_fields[col]
                 raw_val = new_values.get(record.id, {}).get(col)
-                new_val_cached = field.convert_to_cache(raw_val, record) if raw_val is not None else None
+                new_val_cached = field.convert_to_cache(str(raw_val), record) if raw_val is not None else None
 
                 if new_val_cached is None:
                     continue
@@ -383,7 +383,7 @@ def patched_flush(self, fnames=None):
         # No saved old values, need to query all from database
         for name, field in loggable_fields_dict.items():
             for rid in batches.get(name, []):
-                d[field].append(rid)
+                missing_queries[field].append(rid)
 
     # Query database for missing old values
     for field, miss_ids in missing_queries.items():
