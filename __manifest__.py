349f3c6308bdb96c6f31a4970bd304a0ef7d63a7
Ries Vriend <ries@vriend.com>
2025-09-04 14:26:25 +0200

    build

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "oteny_audit",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "depends": ["base", "mail"],
     "author": "Oteny.com",
     "category": "Extra Tools",
