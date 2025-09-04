4f8a0d8272ae451fe29c39701427a4c2374904fc
Ries Vriend <ries@vriend.com>
2025-09-04 14:56:24 +0200

    version audit also

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "18.0.1.833",
+    "version": "18.0.1.837",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
