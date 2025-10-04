40f97c93d5704a665030119e0faa21a065834540
Ries Vriend <ries@vriend.com>
2025-10-04 15:45:31 +0200

    - radar calendar double click opens related service/entry, not state record - creating services in the radar calendar now possible, also for multiple days

--- static/src/components/state_record_calendar/state_record_calendar_view.js
+++ static/src/components/state_record_calendar/state_record_calendar_view.js
@@ -3,9 +3,11 @@
 import { calendarView } from "@web/views/calendar/calendar_view";
 import { registry } from "@web/core/registry";
 import { StateRecordCalendarRenderer } from "./state_record_calendar_renderer";
+import { StateRecordCalendarController } from "./state_record_calendar_controller";
 
 export const stateRecordCalendarView = {
     ...calendarView,
+    Controller: StateRecordCalendarController,
     Renderer: StateRecordCalendarRenderer,
 };
 
