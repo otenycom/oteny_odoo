0f02071a0fdeda227a25e8d22a2cb872f0bc94f4
Ries Vriend <ries@vriend.com>
2025-11-02 09:57:22 +0100

    Deploy feature/planning to test2: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "19.0.1.913",
+    "version": "19.0.1.914",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
