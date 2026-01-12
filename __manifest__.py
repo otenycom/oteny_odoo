8509418ae577a06979e9c947d8db74c369883452
Ries Vriend <ries@vriend.com>
2026-01-12 14:56:24 +0100

    Deploy feature/salaries to test2: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "19.0.1.950",
+    "version": "19.0.1.951",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
