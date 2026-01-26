26859309d58d0dcf0d68d1eac90c3d47e4a87797
Thijs van Waaij <thijs@vriend.studio>
2026-01-26 12:26:17 +0100

    Deploy code to test1: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "oteny_audit",
-    "version": "19.0.1.116",
+    "version": "19.0.1.117",
     "depends": ["base", "mail"],
     "author": "Oteny.com",
     "category": "Extra Tools",
