0bc1e70a5a1f4ca81ec0505915d6ca55385d1fea
Ries Vriend <ries@vriend.com>
2026-02-08 10:47:30 +0100

    Deploy code to test1: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "oteny_audit",
-    "version": "19.0.1.124",
+    "version": "19.0.1.125",
     "depends": ["base", "mail"],
     "author": "Oteny.com",
     "category": "Extra Tools",
