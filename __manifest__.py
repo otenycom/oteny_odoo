f25f08dca1e5c143aefe762be768589299ebcf59
Ries Vriend <ries@vriend.com>
2026-02-13 17:05:12 +0100

    Deploy code to test1: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "19.0.1.983",
+    "version": "19.0.1.984",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
