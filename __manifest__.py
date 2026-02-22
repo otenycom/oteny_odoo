8b30dd27fe75928f974f09e166e9e35b39b1c88a
Ries Vriend <ries@vriend.com>
2026-02-22 22:43:28 +0100

    Deploy feature/wilma to test2: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "19.0.1.998",
+    "version": "19.0.1.999",
     "depends": ["base", "mail", "documents"],
     "author": "Vriend Studio",
     "category": "Workflow",
