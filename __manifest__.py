dbe59766024ee87609e99a6777dc79591ee4e1a4
Ries Vriend <ries@vriend.com>
2026-02-26 23:52:06 +0100

    Deploy feature/wilma to test2: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "oteny_audit",
-    "version": "19.0.1.164",
+    "version": "19.0.1.165",
     "depends": ["base", "mail"],
     "author": "Oteny.com",
     "category": "Extra Tools",
