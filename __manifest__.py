1cae01238fb72bfa121b513e8792e3fbcc875fc9
Ries Vriend <ries@vriend.com>
2026-03-31 21:13:41 +0200

    Deploy feature/hr to test2: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "oteny_audit",
-    "version": "19.0.1.250",
+    "version": "19.0.1.252",
     "depends": ["base", "mail"],
     "author": "Oteny.com",
     "category": "Extra Tools",
