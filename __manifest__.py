c321d757ef7854715898131d899c54857715acf9
Ries Vriend <ries@vriend.com>
2026-03-31 21:13:41 +0200

    Deploy feature/hr to test2: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "19.0.1.1075",
+    "version": "19.0.1.1076",
     "depends": ["base", "mail", "documents", "oteny_shortcut"],
     "author": "Vriend Studio",
     "category": "Workflow",
