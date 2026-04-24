c6065a7e88c778763c254c98c7b7b710e68abff1
riesvriend <ries@vriend.com>
2026-04-25 00:18:18 +0200

    Deploy feature/nasimport2 to test2: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Riverflow",
-    "version": "19.0.1.1147",
+    "version": "19.0.1.1148",
     "depends": ["base", "mail", "documents", "oteny_shortcut"],
     "author": "Vriend Studio",
     "category": "Workflow",
