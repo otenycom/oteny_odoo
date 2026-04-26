1460b7d0f8f55ae80d139a486ff310f0cc921aa6
riesvriend <ries@vriend.com>
2026-04-26 16:26:03 +0200

    Deploy feature/nasimport2 to test2: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "oteny_audit",
-    "version": "19.0.1.404",
+    "version": "19.0.1.406",
     "depends": ["base", "mail"],
     "author": "Oteny.com",
     "category": "Extra Tools",
