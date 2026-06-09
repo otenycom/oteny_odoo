79d87b9fb84cf02137e4ac4e2622280710660c09
Thijs van Waaij <thijs@vriend.studio>
2026-06-09 15:20:20 +0200

    Deploy feature/work-permit-v2 to test2: increment version

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "oteny_audit",
-    "version": "19.0.1.464",
+    "version": "19.0.1.466",
     "depends": ["base", "mail"],
     "author": "Oteny.com",
     "category": "Extra Tools",
