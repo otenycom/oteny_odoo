23bf677f796aae3a8af15149a1eccffcfbde2d9b
riesvriend <ries@vriend.com>
2026-09-18 16:46:04 +0200

    Deploy dev to test1: increment version

--- odoo_parallel_tests/__manifest__.py
+++ odoo_parallel_tests/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Parallel Test Runner",
-    "version": "19.0.1.2",
+    "version": "19.0.1.3",
     "author": "Oteny",
     "category": "Technical",
     "summary": "Parallelizes Odoo test execution by cloning the test database and running batches in worker subprocesses",
